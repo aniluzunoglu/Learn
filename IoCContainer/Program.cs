85f7c5d75debfebf7999a27b2b0cb30fcbb7f676
aniluzunoglu <aniluzunoglu@outlook.com>
2021-05-08 17:20:23 +0300

    IoC container ninject simple usage example

---
+++ IoCContainer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ninject;
+
+namespace IoCContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IoC container 
+            // we put instances into kernel box
+            var kernel = new StandardKernel();
+            kernel.Bind<ILegislation>().To<SecondLegislation>().InSingletonScope();
+            // if someone wants ILegislation give him SecondLecislation.
+            // it is usually used in transaction between layers
+
+            CustomerManager customerManager = new CustomerManager(kernel.Get<ILegislation>());
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IoCContainer/SecondLegislation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoCContainer
+{
+    class SecondLegislation : ILegislation
+    {
+        public void Perform()
+        {
+            Console.WriteLine("customer added according to second legislation");
+        }
+    }
+}
